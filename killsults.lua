d779b369b7a03292cbf9805643ac47dec765eacd
minecraftfornpm <abobatopaha@gmail.com>
2026-02-18 13:46:24 +0400

    Create killsults.lua

---
+++ killsults.lua
@@ -0,0 +1,15 @@
+return {
+	"{plr} not really",
+	"{plr} you know how play?",
+	"hey {plr} use l1recode",
+	"{plr} = free kills",
+	"{plr} im lagging, no cheats",
+	"bad skills, {plr}",
+	"{plr} just using L1recode'd",
+	"{plr}, you are way worse than AquaVClip",
+	"Stav is the 🐐, {plr}",
+	"W Breter98!, right {plr}?",
+	"Rhoma taught me how to play, {plr}",
+  "pls {plr} use l1recode",
+  "{plr} why report? LOL"
+}
