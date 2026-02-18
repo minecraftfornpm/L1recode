91fab1e8729c96227fec1317022fc0031ed06fcb
minecraftfornpm <abobatopaha@gmail.com>
2026-02-18 16:46:27 +0400

    Delete Loader.lua

--- Loader.lua
+++
@@ -1,5 +0,0 @@
-repeat task.wait() until game:IsLoaded()
-if game.PlaceId == 11630038968 or game.PlaceId == 10810646982 or game.PlaceId == 139566161526375 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/minecraftfornpm/L1recode/refs/heads/main/main.lua"))()
-	game:GetService("Chat"):Chat(game.Players.LocalPlayer.Character:WaitForChild("Head"), "test cheak...")
-end
